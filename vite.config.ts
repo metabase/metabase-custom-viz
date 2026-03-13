5a19bfe3aef1b63d99417893c62d12d6f3254c00
Uladzimir Havenchyk <uladzimir@metabase.com>
2026-03-13 10:56:18 +0300

    upgrade vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,25 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
+  plugins: [
+    dts({
+      tsconfigPath: resolve(__dirname, "tsconfig.lib.json"),
+    }),
+  ],
   build: {
     target: "node20",
     outDir: "dist",
     lib: {
-      entry: resolve(__dirname, "src/cli.ts"),
+      entry: {
+        cli: resolve(__dirname, "src/cli.ts"),
+        index: resolve(__dirname, "src/index.ts"),
+      },
       formats: ["es"],
-      fileName: "cli",
     },
-    rollupOptions: {
-      external: ["commander"],
+    rolldownOptions: {
+      external: ["commander", "react"],
     },
   },
 });
